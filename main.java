f5957f46b1ffd8fa61e3087f223f4dbd2db275eb
chandram13 <mschandr@uci.edu>
2022-08-08 11:45:15 -0700

    Updated calculate car payment function with starting credit scores.

--- main.java
+++ main.java
@@ -1,4 +1,4 @@
-// Marvish Chandra
+
 import math
 
 public class AffordingAssets {
@@ -7,9 +7,32 @@ public class AffordingAssets {
         r = (interest / 100) / 12
         n = 12 * years
         mortgage = loan * interest * (1 + r)^n / ((1 + r)^n - 1))
+        totalRepay = m * n
+        totalInterest = totalRepay - loan
     }
 
-    public static void calculateCar (String [] args){
+    public calculateCar (String [] excellentScore,greatScore,verygoodScore,goodScore,fairScore,poorScore,verypoorScore,extremelypoorScore,cashDown,years){
+        // credit score arguments are for set interests
+        excellentScore = int;
+        greatScore = int;
+        verygoodScore = int;
+        goodScore = int;
+        fairScore = int;
+        poorScore = int;
+        verypoorScore = int;
+        r = (interest / 100) /12
+        n = 12 * years
+        totalRepay = m * n
+        totalInterest = totalRepay - loan
+        mortgage = loan * interest * (1 + r)^n / ((1 + r)^n - 1)
+        esp1 = loan * interest * (1 + r)^n / ((1 + r)^n - 1) - cashDown
+        System.outPrintln("Your first month's car payment (with excellent credit) is" + esp1 + "discounted by your first down payment.")
+        esp =  loan * interest * (1 + r)^n / ((1 + r)^n - 1)
+        System.outPrintln("Your regular car payment is (with excellent credit) is" + esp1 + ".")
 
+        gsp1 = loan * interest * (1 + r)^n / ((1 + r)^n - 1) - cashDown
+        System.outPrintln("Your first month's car payment (with great credit) is" + gsp1 + "discounted by your first down payment.")
+        gsp =  loan * interest * (1 + r)^n / ((1 + r)^n - 1)
+        System.outPrintln("Your regular car payment is (with great credit) is" + gsp + ".")
     }
 }
