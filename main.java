cbd6159cf3ab66909500867b57f50828d2c97cbc
chandram13 <mschandr@uci.edu>
2022-08-08 11:18:55 -0700

    Updated n variable

--- main.java
+++ main.java
@@ -5,7 +5,8 @@ public class AffordingAssets {
 
     public static void calculateMortgage(String [] loan,interest,years){
         r = (interest / 100) / 12
-        mortgage = loan * interest * (1 + r)^years / ((1 + r)^years - 1))
+        n = 12 * years
+        mortgage = loan * interest * (1 + r)^n / ((1 + r)^n - 1))
     }
 
     public static void calculateCar (String [] args){
