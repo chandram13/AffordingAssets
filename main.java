abfe29a5b4950bab5447c2bb66a610fb896ca07e
chandram13 <mschandr@uci.edu>
2022-08-08 11:16:54 -0700

    Updated the mortgage function to calculate a payment given particular values.

--- main.java
+++ main.java
@@ -1,2 +1,13 @@
-public class main {
+import math
+
+public class AffordingAssets {
+
+    public static void calculateMortgage(String [] loan,interest,years){
+        r = (interest / 100) / 12
+        mortgage = loan * interest * (1 + r)^years / ((1 + r)^years - 1))
+    }
+
+    public static void calculateCar (String [] args){
+
+    }
 }
